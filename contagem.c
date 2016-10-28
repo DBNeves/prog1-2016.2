bea948eead22b379802f4b147aa3dc420d808eaf
Daniel Baeta Neves <daniprax3@gmail.com>
2016-10-28 21:01:36 -0200

    Add files via upload

---
+++ contagem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+ int cont;
+ printf("\nInicio da contagem \n");
+ for(cont=0; cont<=50; cont++)
+ {
+     printf("%d, ", cont); 
+ 
+ }
+ 
+ printf("\nFim da contagem \n");
+ 
+ system("pause");
+return 0;
+}
+
+
